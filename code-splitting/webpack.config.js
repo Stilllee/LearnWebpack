7deed5966235691aa031553045a38bff9fd971b3
Stilllee <orodae@gmail.com>
2024-11-02 01:21:38 +0900

    주요 속성의 이해

---
+++ code-splitting/webpack.config.js
@@ -0,0 +1,18 @@
+var path = require("path");
+
+module.exports = {
+  mode: "none",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
