9e9287f65a846daefda2480f0f7ad7f877c17ca3
Stilllee <orodae@gmail.com>
2024-11-01 23:38:48 +0900

    웹팩 적용 전

---
+++ getting-started/src/index.js
@@ -0,0 +1,10 @@
+function component() {
+  var element = document.createElement("div");
+
+  /* 다음 줄이 작동하려면 lodash가 필요합니다. */
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+  return element;
+}
+
+document.body.appendChild(component());
