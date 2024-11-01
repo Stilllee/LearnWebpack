44f3077d81e8f343818ae07c153a792bb0c5900f
Stilllee <orodae@gmail.com>
2024-11-02 01:29:53 +0900

    웹팩 설정 파일 분석

--- code-splitting/webpack.config.js
+++ code-splitting/webpack.config.js
@@ -1,17 +1,17 @@
 var path = require("path");
 
 module.exports = {
-  mode: "none",
+  mode: "none", // production(배포), development, none
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    filename: "[chunkhash].js",
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.css$/, // 모든 css 파일을 대상으로 함
+        use: ["style-loader", "css-loader"], // 사용할 로더
       },
     ],
   },
