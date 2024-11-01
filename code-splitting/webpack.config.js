3f9a1d1934d716e9676df696470baf207dfcb7e2
Stilllee <orodae@gmail.com>
2024-11-02 01:35:00 +0900

    웹팩의 로더 적용 순서

--- code-splitting/webpack.config.js
+++ code-splitting/webpack.config.js
@@ -5,13 +5,13 @@ module.exports = {
   entry: "./index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[chunkhash].js",
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.css$/, // 모든 css 파일을 대상으로 함
-        use: ["style-loader", "css-loader"], // 사용할 로더
+        use: ["style-loader", "css-loader"], // 사용할 로더: 오른쪽에서 왼쪽의 순서로 적용되니 작성에 유의할 것!
       },
     ],
   },
